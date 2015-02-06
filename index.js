0712d5520b6ffd5bae547f7cc3f2b1cdcfd15d3e
Justin Sippel <justin.sippel@urbanairship.com>
2015-02-06 10:25:43 -0800

    Comments and reformatting

--- index.js
+++ index.js
@@ -14,16 +14,22 @@ function deepEqual(a, b) {
     return isNan(a) && isNan(b)
   }
 
+  // If typeof is not 'object', then it's either a primitive
+  // or a function. In each case, if they're not triple-equal
+  // then they're not equal.
   if(typeof a !== 'object' || typeof b !== 'object') {
     return false
   }
 
+  // If one is an array and the other isn't, not equal
   if(Array.isArray(a) || Array.isArray(b)) {
     if(!Array.isArray(a) || !Array.isArray(b)) {
       return false
     }
   }
 
+  // If both are Dates, compare time since epoch.
+  // If one is a Date and the other isn't, not equal
   if(a instanceof Date || b instanceof Date) {
     if(a instanceof Date && b instanceof Date) {
       return a.getTime() === b.getTime()
@@ -32,6 +38,8 @@ function deepEqual(a, b) {
     return false
   }
 
+  // If one is a RegExp and the other isn't, not equal.
+  // If both are RegExps, compare them as patterns.
   if(a instanceof RegExp || b instanceof RegExp) {
     if(a instanceof RegExp && b instanceof RegExp) {
       return RegExp.prototype.toString.call(a) ===
@@ -45,6 +53,8 @@ function deepEqual(a, b) {
     return buffersEqual(a, b)
   }
 
+  // If one is an arguments object and the other isn't, not equal.
+  // If both are arguments objects, compare them as arrays.
   if(isArguments(a) || isArguments(b)) {
     if(!isArguments(a) || !isArguments(b)) {
       return false
@@ -53,6 +63,9 @@ function deepEqual(a, b) {
     return deepEqual([].slice.call(a), [].slice.call(b))
   }
 
+  // Walk the keys of both objects and compare them recursively.
+  // If we find a difference, they're not equal. If no differences
+  // are found, equal.
   aKeys = Object.keys(a).sort()
   bKeys = Object.keys(b).sort()
 
@@ -75,6 +88,7 @@ function deepEqual(a, b) {
   return true
 }
 
+// NaN is the only value not-equal to itself
 function isNan(v) {
   return v !== v
 }
@@ -84,9 +98,14 @@ function isArguments(o) {
 }
 
 function isBuffer(x) {
-  if(!x || typeof x !== 'object' || typeof x.length !== 'number' ||
-      typeof x.copy !== 'function' || typeof x.slice !== 'function') {
+  if(!x || typeof x !== 'object') {
     return false
+  }
+
+  if(typeof x.length !== 'number' ||
+      typeof x.copy !== 'function' ||
+      typeof x.slice !== 'function') {
+        return false
   }
 
   if(x.length > 0 && typeof x[0] !== 'number') {
@@ -96,6 +115,9 @@ function isBuffer(x) {
   return true
 }
 
+// If one is a buffer and the other isn't, not equal.
+// Otherwise, compare lengths, then compare values
+// at each index.
 function buffersEqual(a, b) {
   if(!isBuffer(a) || !isBuffer(b)) {
     return false
